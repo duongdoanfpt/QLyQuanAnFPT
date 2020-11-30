cdb04f11a13f33cc905a88c2729a2d06ed77eae0
duongdoanfpt <59642215+duongdoanfpt@users.noreply.github.com>
2020-11-30 17:20:26 +0700

    Duong Upate - Hinh NhanVien
    Update hình NhanVien

--- DingDoongSoft/GUI_DingDoong/FormNhanVien.Designer.cs
+++ DingDoongSoft/GUI_DingDoong/FormNhanVien.Designer.cs
@@ -248,8 +248,7 @@ namespace GUI_DingDoong
             // 
             // pbHinh
             // 
-            this.pbHinh.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbHinh.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.pbHinh.BackColor = System.Drawing.Color.Gainsboro;
             this.pbHinh.Cursor = System.Windows.Forms.Cursors.Hand;
             this.pbHinh.Location = new System.Drawing.Point(3, 3);
@@ -257,6 +256,7 @@ namespace GUI_DingDoong
             this.pbHinh.Size = new System.Drawing.Size(221, 136);
             this.pbHinh.TabIndex = 0;
             this.pbHinh.TabStop = false;
+            this.pbHinh.Click += new System.EventHandler(this.pbHinh_Click);
             // 
             // dgvNhanVien
             // 
@@ -318,7 +318,7 @@ namespace GUI_DingDoong
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 1042F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 1045F));
             this.tableLayoutPanel1.Controls.Add(this.pbThongKe, 4, 0);
             this.tableLayoutPanel1.Controls.Add(this.pbBan, 3, 0);
             this.tableLayoutPanel1.Controls.Add(this.pbKhachHang, 2, 0);
@@ -367,7 +367,7 @@ namespace GUI_DingDoong
             this.lblUsers.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.lblUsers.AutoSize = true;
             this.lblUsers.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblUsers.Location = new System.Drawing.Point(1145, 9);
+            this.lblUsers.Location = new System.Drawing.Point(1148, 9);
             this.lblUsers.Name = "lblUsers";
             this.lblUsers.Size = new System.Drawing.Size(162, 21);
             this.lblUsers.TabIndex = 6;
